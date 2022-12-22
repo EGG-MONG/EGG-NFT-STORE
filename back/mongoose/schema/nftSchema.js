03f81b0cf143204f0e1928d69841ed56425c7bb1
YounmiSon <symym629@gmail.com>
2022-12-22 15:14:03 +0900

    스키마 수정

--- back/mongoose/schema/nftSchema.js
+++ back/mongoose/schema/nftSchema.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
-const nftSchema = new mongoose.Schema({
-  name: String,
-  owner: String,
-  maker: String,
-  description: String,
-  image: String,
-  edition: Number,
-  date: Number,
-  attribute: [
-    {
-      trait_type: String,
-      value: String,
-    },
-  ],
-  transactions: [transactionSchema.schema],
-  transfer: [transferSchema.schema],
-});
+const nftSchema = new mongoose.Schema(
+  {
+    name: String,
+    owner: String,
+    maker: String,
+    description: String,
+    image: String,
+    edition: Number,
+    date: Number,
+    state: String,
+    price: Number,
+    attribute: [
+      {
+        trait_type: String,
+        value: String,
+      },
+    ],
+    transactions: [transactionSchema.schema],
+    transfer: [transferSchema.schema],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Nft", nftSchema);
--- back/mongoose/schema/transferSchema.js
+++ back/mongoose/schema/transferSchema.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose');
-const transferSchema = new mongoose.Schema({
-    transactionHash : String,
-    type : String,
-    price : Number,
-    from : String,
-    to : String
-});
-module.exports = mongoose.model('Transfer', transferSchema);
+const mongoose = require("mongoose");
+const transferSchema = new mongoose.Schema(
+  {
+    transactionHash: String,
+    type: String,
+    price: Number,
+    from: String,
+    to: String,
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Transfer", transferSchema);
