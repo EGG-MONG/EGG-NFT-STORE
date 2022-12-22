15a636c72215365d7f4d62afc0901c927d84b07b
YounmiSon <symym629@gmail.com>
2022-12-22 11:35:06 +0900

    web3 설정, 메타마스크 계정 연결

--- front/pages/mypage.js
+++ front/pages/mypage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import useWeb3 from "./hooks/useWeb3";
 
 const mypage = () => {
+  const [web3, account] = useWeb3();
+  if (!account) return <h1>메타마스크를 연결해주세요</h1>;
+
   return (
     <>
       <div>
-        <Address>1</Address>
+        <Address>계정 : {account}</Address>
         <MenuWrap></MenuWrap>
       </div>
     </>
@@ -15,7 +19,7 @@ const Address = styled.div`
   width: inherit;
   padding-top: 3rem;
   padding-left: 3rem;
-  font-size: 4rem;
+  font-size: 2rem;
   font-weight: 800;
 `;
 
