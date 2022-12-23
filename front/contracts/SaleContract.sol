0662da27c67c80b413303a32ed3ae712b1018548
nataya1116 <nataya1116@gmail.com>
2022-12-23 11:41:17 +0900

    [front] SaleToken -> SaleContract 이름 변경

--- front/contracts/SaleContract.sol
+++ front/contracts/SaleContract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import "./EggToken.sol";
 
-contract SaleToken {
+contract SaleContract {
     // EggToken을 담을 상태변수
     EggToken public Token;
 
@@ -16,7 +16,7 @@ contract SaleToken {
         uint price;
     }
 
-    uint[] public SaleTokenList;
+    uint[] public SaleContractList;
 
     // 판매 등록 이벤트 등록
     event List(uint tokenId, string state, uint price, address from, address to);
@@ -33,7 +33,7 @@ contract SaleToken {
     }
 
     // 판매 등록 함수
-    function ListFotSaleToken(uint _tokenId, uint _price) public {
+    function ListFotSaleContract(uint _tokenId, uint _price) public {
         address tokenOwner = Token.ownerOf(_tokenId);
 
         // 판매가격이 0 이하면 함수 종료
@@ -48,7 +48,7 @@ contract SaleToken {
         tokenPrices[_tokenId] = _price;
 
         // 판매 리스트 배열에 추가
-        SaleTokenList.push(_tokenId);
+        SaleContractList.push(_tokenId);
 
         emit List(_tokenId, "List", _price, msg.sender, address(0));
     }
@@ -75,20 +75,20 @@ contract SaleToken {
         // 판매 가격이 0으로 변경해 판매중지 상태로 변경한다.
         tokenPrices[_tokenId] = 0;   
 
-        popSaleToken(_tokenId); 
+        popSaleContract(_tokenId); 
 
         emit Sale(_tokenId, "Sale", msg.value, tokenOwner, msg.sender);
         emit Transfer(_tokenId, "Transfer", 0, tokenOwner, msg.sender);
     }
 
-    function popSaleToken(uint _tokenId) private returns (bool) {
-        for (uint256 i = 0; i < SaleTokenList.length; i++) {
-            if(SaleTokenList[i] == _tokenId){
+    function popSaleContract(uint _tokenId) private returns (bool) {
+        for (uint256 i = 0; i < SaleContractList.length; i++) {
+            if(SaleContractList[i] == _tokenId){
                 // 마지막에 저장되어 있던 요소를 삭제해야할 인덱스에 저장
-                SaleTokenList[i] = SaleTokenList[SaleTokenList.length - 1];
+                SaleContractList[i] = SaleContractList[SaleContractList.length - 1];
 
                 // 배열의 마지막 요소 삭제
-                SaleTokenList.pop();
+                SaleContractList.pop();
                 return true;
             }
         }
@@ -96,15 +96,15 @@ contract SaleToken {
     }
     
     // 전체 판매 리스트 확인 view
-    function getSaleTokenList() public view returns (TokenInfo[] memory){
+    function getSaleContractList() public view returns (TokenInfo[] memory){
         
         // 판매 리스트가 한 개 이상일 경우에만 실행
-        require(SaleTokenList.length > 0);
+        require(SaleContractList.length > 0);
         
-        TokenInfo[] memory list = new TokenInfo[](SaleTokenList.length);
+        TokenInfo[] memory list = new TokenInfo[](SaleContractList.length);
 
-        for (uint256 i = 0; i < SaleTokenList.length; i++) {
-            uint tokenId = SaleTokenList[i];
+        for (uint256 i = 0; i < SaleContractList.length; i++) {
+            uint tokenId = SaleContractList[i];
             uint price = tokenPrices[tokenId];
             // 토큰 인포 구조체 생성해서 저장
             list[i] = TokenInfo(tokenId, price);
