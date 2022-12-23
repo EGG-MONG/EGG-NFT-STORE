58f6c02231529b8b5fdea18bf97dc17a5fe44922
nataya1116 <nataya1116@gmail.com>
2022-12-23 17:28:39 +0900

    [front] 타입 수정(uint256 -> uint8)

--- front/contracts/EggToken.sol
+++ front/contracts/EggToken.sol
@@ -7,10 +7,11 @@ import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
 
 import "../node_modules/openzeppelin-solidity/contracts/utils/Strings.sol";
 
+
 contract EggToken is ERC721Enumerable, Ownable {
-    string public a;
+
     // 최대 발행량
-    uint constant public MAX_TOKEN_COUNT = 500;
+    uint8 constant public MAX_TOKEN_COUNT = 100;
 
     // 민팅 시 지불할 이더 가격을 나타내주는 상태변수
     uint public mintPrice = 10 wei;
@@ -18,21 +19,27 @@ contract EggToken is ERC721Enumerable, Ownable {
     // json metadata URI
     string public metadataURI;
 
-    // minting 이벤트 등록
+    // 발행된 토큰 목록
+    uint8[] tokenList;
 
-    event Minting(uint tokenId, string state, uint price, address from, address to);
+    // minting 이벤트 등록
+    event Minting(uint8 tokenId, string state, uint price, address from, address to);
 
     // 생성자 함수
     constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721 (_name, _symbol) {
         metadataURI = _metadataURI;
     }
 
-    function mintToken(address _saleContractCA, uint _tokenId) public payable {
-        // mintPrice보다 지불하려는 가격이 이상일 경우에만 실행한다
-        require(msg.value >= mintPrice);
+    function mintToken(address _saleContractCA, uint8 _tokenId) public payable {
 
-        // 최대발행량이 현재 총발행량보다 클 때 발행한다.
-        require(MAX_TOKEN_COUNT > totalSupply());
+        // 중복되는 tokenId가 존재하지 않을 경우에만 발행한다.
+        require(_overlapCheckTokenId(_tokenId), "EggToken : It's a token that already exists");
+
+        // mintPrice보다 지불하려는 가격이 이상일 경우에만 발행한다
+        require(msg.value >= mintPrice,"EggToken : Be short of money");
+
+        // 최대발행량이 현재 총발행량보다 클 때만 발행한다.
+        require(MAX_TOKEN_COUNT > totalSupply(), "EggToken : It exceeded the maximum amount issued");
 
         // CA에 지급받은 이더를 전송해준다.
         payable(Ownable.owner()).transfer(msg.value);
@@ -42,7 +49,18 @@ contract EggToken is ERC721Enumerable, Ownable {
 
         setApprovalForAll(_saleContractCA, true);
 
+        tokenList.push(_tokenId);
+
         emit Minting(_tokenId, "Minted", 0, address(0), msg.sender);
+    }
+
+    function _overlapCheckTokenId(uint8 _tokenId) private view returns (bool){
+        if(tokenList.length == 0) return true;
+        //  토큰 갯수는 최대 100개이므로 0~255의 범위를 가진 uint8을 사용한다.
+        for(uint8 i = 0; i < tokenList.length; i++){
+            if(tokenList[i] == _tokenId) return false;
+        }
+        return true;
     }
 
     function tokenURI(uint _tokenId) public override view returns (string memory) {
@@ -53,5 +71,5 @@ contract EggToken is ERC721Enumerable, Ownable {
 
     function setMetadataURI(string memory _uri) public onlyOwner {
         metadataURI = _uri;
-    }
+    } 
 }
