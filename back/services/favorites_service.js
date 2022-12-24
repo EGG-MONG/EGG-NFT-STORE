092ab6ad6197a5326862782ef52202771efbd806
sujin <nataya1116@gmail.com>
2022-12-24 20:02:16 +0900

    [back]favorites 서비스 수정

--- back/services/favorites_service.js
+++ back/services/favorites_service.js
@@ -1,10 +1,9 @@
-const { Favorites } = require("../mongoose/schema");
+const { Favorites } = require("../model/schema");
 
-module.exports.insertOrUpdate = async (favoritesObj) => {
+module.exports.insertOrUpdate = async (wallet, favorites) => {
     try {
-        const { wallet, favorites } = favoritesObj;
         //                               filter , update     
-        return Favorites.findOneAndUpdate(wallet, favorites, {
+        return Favorites.findOneAndUpdate({wallet}, {favorites}, {
             new: true, // 적용된 문서 반환 옵션
             upsert: true // 문서가 있을경우 업데이트 없을 경우 생성
         });
@@ -14,13 +13,22 @@ module.exports.insertOrUpdate = async (favoritesObj) => {
     }
 }
 
-module.exports.getList = async () => {
+module.exports.getList = async (wallet) => {
     try {
         // 전체조회해서 정렬을 _id 기준으로 역순
-        return Favorites.find();
+        return Favorites.findOne({wallet}).exec();
     } catch (error) {
         console.error(error);
         return false;
     }
 }
 
+module.exports.update = async (wallet, favorites) => {
+    try {
+        //                      filter , update     
+        return Favorites.update({wallet}, {favorites});
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
