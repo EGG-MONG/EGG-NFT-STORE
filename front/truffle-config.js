dfa49c66352320d71051000d689f9fa369e94e3e
sujin <nataya1116@gmail.com>
2022-12-25 18:49:19 +0900

    [front] truffle goreil 네트워크 설정 중

--- front/truffle-config.js
+++ front/truffle-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+// npx truffle migrate --network goerli
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +9,11 @@ module.exports = {
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "1337", // Any network (default: none)
     },
+    // goerli: {
+    //   provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_API_KEY),
+    //   network_id: '5',
+    //   gas: 4465030
+    // }
   },
 
   compilers: {
