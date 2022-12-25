c29a57250e303830b93021ad6f40eddde10d8a6d
sujin <nataya1116@gmail.com>
2022-12-26 05:33:50 +0900

    [back] image 경로 오타 수정

--- front/src/redux/contractReducer.js
+++ front/src/redux/contractReducer.js
@@ -3,6 +3,8 @@ import produce from 'immer';
 import Web3 from "web3/dist/web3.min";
 import EggToken from "../contracts/EggToken.json";
 import SaleContract from "../contracts/SaleContract.json";
+import { addNft, modifyNft } from "./nftReducer";
+import { ethers } from "ethers";
 
 let eventCheck = false;
 
@@ -18,8 +20,9 @@ function getContract(){
     });
 
     // 지갑
-    const account = address;
-    // console.log({account});
+    const { utils } = ethers;
+    const account = utils.getAddress(address);
+    console.log({account});
 
     const networkId = await web3.eth.net.getId();
     // console.log({networkId});
@@ -28,47 +31,79 @@ function getContract(){
     // console.log({eggCA});
 
     const saleCA = SaleContract.networks[networkId].address;
-    // console.log({saleCA});
+    console.log({saleCA});
     
     const eggDeployed = new web3.eth.Contract(EggToken.abi, eggCA);
     // console.log({eggDeployed});
 
     const saleDeployed = new web3.eth.Contract(SaleContract.abi, saleCA);
-    // console.log({saleDeployed});
+    console.log({saleDeployed});
     
-    if(!eventCheck) eventSubscribe(dispatch, eggDeployed, saleDeployed);
+    if(!eventCheck) eventSubscribe(dispatch, utils, web3, eggDeployed, saleDeployed);
 
     dispatch({ type: CONTRACT_GET, payload: { web3, account, networkId, eggCA, eggDeployed, saleCA, saleDeployed } });
   }
 }
 
-async function eventSubscribe(dispatch, eggDeployed, saleDeployed) {
+async function eventSubscribe(dispatch, utils, web3, eggDeployed, saleDeployed) {
   console.log("eventSubscribe");
   await window.ethereum.on("accountsChanged", (accountArr) => {
-    const account = accountArr[0];
+    const account = utils.getAddress(accountArr[0]);
     console.log({ accountsChanged: account });
     dispatch({ type: ACCOUT_CHANGE, payload: { account } });
     // dispatch(getFavoritesList(account));
   });
 
   await window.ethereum.on("disconnect", (accountArr) => {
-    const account = accountArr[0];
+    const account = utils.getAddress(accountArr[0]);
     console.log({ disconnect: account });
     dispatch({ type: ACCOUT_CHANGE, payload: { account } });
     // dispatch(getFavoritesList(account));
   });
 
-  await eggDeployed.events.allEvents(() => {
-  }).on("connected", function(subscriptionId){
-      console.log('SubID: ',subscriptionId);
-  })
-  .on('data', function(event){
-      console.log('Event:', event);
-      console.log('returnValues: ',event.returnValues);
-      //Write send email process here!
-  })
+  await eggDeployed.events.Minting(() => {
+  }).on('data', async function(event){
+
+      const tokenURI = init.eggToken.metadataURI+"/"+event.returnValues.tokenId+".json";
+      console.log({tokenURI});
+
+      const transaction = await web3.eth.getTransaction(event.transactionHash);
+      console.log({transaction});
+
+      const { tokenId, state, price, from, to } = event.returnValues;
+
+      const transfer = { tokenId, state, price, from, to };
+      console.log({transfer});
+      dispatch(addNft(tokenURI, transaction, transfer));
+  });
+
+  await saleDeployed.events.List(() => {
+  }).on('data', async function(event){
+      eventDeployed(event, web3, dispatch);
+  });
+
+  await saleDeployed.events.Sale(() => {
+  }).on('data', async function(event){
+      eventDeployed(event, web3, dispatch);
+  });
+
+  await saleDeployed.events.Transfer(() => {
+  }).on('data', async function(event){
+      eventDeployed(event, web3, dispatch);
+  });
 
   eventCheck = true;
+}
+
+const eventDeployed = async (event, web3, dispatch) => {
+  const transaction = await web3.eth.getTransaction(event.transactionHash);
+      console.log({transaction});
+
+      const { tokenId, state, price, from, to } = event.returnValues;
+
+      const transfer = { tokenId, state, price, from, to };
+      console.log({transfer});
+      dispatch(modifyNft(tokenId, transaction, transfer));
 }
 
 export { getContract, };
@@ -80,7 +115,7 @@ const init = {
     eggToken : {
       name : "Egg Token",
       symbol : "ETK",
-      metadataURI : "https://gateway.pinata.cloud/ipfs/QmPhbUs1qkBuh4rakLbyGR256DtkmdYCEnjXecdWxg2F8T",
+      metadataURI : "http://localhost:8000/metadatas",
       CA : null,
       deployed : null
     },
