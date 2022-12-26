ae3a5857cb96d9ee2a02324ad601d913936f446d
YounmiSon <symym629@gmail.com>
2022-12-26 21:20:51 +0900

    [front] 민팅 로딩 페이지 추가

---
+++ front/src/components/LoadMinting.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoadMinting = () => {
+  return (
+    <div>
+      <img
+        src="/Img/loading.gif"
+        alt="loadingImg"
+        className
+        style={{ width: "700px", cursor: "pointer" }}
+      />
+    </div>
+  );
+};
+
+export default LoadMinting;
