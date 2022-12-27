67e837b39bafbd41a5d2744eecbeabb36a556429
YounmiSon <symym629@gmail.com>
2022-12-27 17:32:52 +0900

    [back] mongoose 주소 수정

--- back/model/connect.js
+++ back/model/connect.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports = () => {
   function connect() {
-    mongoose.connect(`mongodb://eggmong:${process.env.DATABASE_PASSWORD_DEV}@127.0.0.1:27017`, { dbName: 'egg_nft_test' }, function(err) {
-      if (err) {
-        console.error('mongodb connection error', err);
+    mongoose.connect(
+      // 여기에 로컬 IP가 아니라 퍼블릭 IP써줘야한다
+      `mongodb://eggmong:${process.env.DATABASE_PASSWORD_DEV}@13.124.79.35:27017`,
+      { dbName: "egg_nft_test" },
+      function (err) {
+        if (err) {
+          console.error("mongodb connection error", err);
+        }
+        console.log("mongodb connected");
       }
-      console.log('mongodb connected');
-    });
+    );
   }
   connect();
-  mongoose.connection.on('disconnected', connect);
-  require('./schema/blockSchema.js');
-  require('./schema/transactionSchema.js');
+  mongoose.connection.on("disconnected", connect);
+  require("./schema/blockSchema.js");
+  require("./schema/transactionSchema.js");
   require("./schema/transferSchema");
-  require("./schema/nftSchema")
-  require("./schema/favoritesSchema")
+  require("./schema/nftSchema");
+  require("./schema/favoritesSchema");
 };
