6ff53d76faa758157585906494d6ba2a1fd870ab
nataya1116 <nataya1116@gmail.com>
2022-12-28 12:17:22 +0900

    cors 설정 변경

--- back/app.js
+++ back/app.js
@@ -5,7 +5,7 @@ var app = express();
 const cors = require("cors");
 
 // !!!!!!!!요청 허용 라우터보다 아래에 설정해주면 cors가 작동하지 않는다.!!!!!!!
-app.use(cors({ origin: "http://3.36.72.252:3000", credentials: true }));
+app.use(cors({ origin: "http://3.36.72.252:80", credentials: true }));
 
 // public 폴더 내의 폴더 및 파일들의 경로를 절대경로로 호출할 수 있게 처리
 app.use(express.static("public"));
