81fe1ed4d96417bd49bc43b0fb0dc072ca8b1a56
YounmiSon <symym629@gmail.com>
2022-12-28 09:30:37 +0900

    ubuntu

--- back/model/connect.js
+++ back/model/connect.js
@@ -5,7 +5,7 @@ module.exports = () => {
   function connect() {
     mongoose.connect(
       // 여기에 로컬 IP가 아니라 퍼블릭 IP써줘야한다
-      `mongodb://eggmong:${process.env.DATABASE_PASSWORD_DEV}@43.200.244.126:27017`,
+      `mongodb://eggmong:${process.env.DATABASE_PASSWORD_DEV}@13.124.78.191:27017`,
       { dbName: "egg_nft_test" },
       function (err) {
         if (err) {
